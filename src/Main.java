b3a8b6ea4f0f052197b033984513ebe37d6fd653
Jeanine Nebrija <915909649@sfsu.edu>
2024-03-30 09:13:27 -0700

    First Commit

--- src/Main.java
+++ src/Main.java
@@ -20,7 +20,7 @@ import java.sql.PreparedStatement;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        Connection connection= MySQLConnection.getDBConnection();
+        Connection connection = MySQLConnection.getDBConnection();
         PreparedStatement pStatement = connection.prepareStatement("DROP schema IF EXISTS bank");
         pStatement.executeUpdate();
         pStatement = connection.prepareStatement("CREATE schema bank");
@@ -43,46 +43,46 @@ public class Main {
         System.out.println("Setting up sample accounts:");
 
         // Account #0 /////////////////////////
-        u=User.parseJson("{\"id\":0,\"userName\":\"Josh\",\"role\":\"CUSTOMER\",\"accounts\":null}");
+        u = User.parseJson("{\"id\":0,\"userName\":\"Josh\",\"role\":\"CUSTOMER\",\"accounts\":null}");
         System.out.println("User created from JSON string; id#" + u.getId());
         userDAO.addUser(u);
-        a=Account.parseJson("{\"id\":0}");
+        a = Account.parseJson("{\"id\":0}");
         System.out.println("Account created from JSON string; id#" + a.getId());
         a.setUser(u);
-        t=new Transaction(0,a,"INITIAL DEPOSIT", BigDecimal.valueOf(1000000), LocalDateTime.now());
+        t = new Transaction(0, a, "INITIAL DEPOSIT", BigDecimal.valueOf(1000000), LocalDateTime.now());
         transactionDAO.addTransaction(t);
         accountDAO.addAccount(a);
 
         // Account #1 /////////////////////////
-        u=User.parseJson("{\"id\":1,\"userName\":\"Dianne\",\"role\":\"CUSTOMER\",\"accounts\":null}");
+        u = User.parseJson("{\"id\":1,\"userName\":\"Dianne\",\"role\":\"CUSTOMER\",\"accounts\":null}");
         System.out.println("User created from JSON string; id#" + u.getId());
         userDAO.addUser(u);
-        a=Account.parseJson("{\"id\":1}");
+        a = Account.parseJson("{\"id\":1}");
         System.out.println("Account created from JSON string; id#" + a.getId());
         a.setUser(u);
-        t=new Transaction(1,a,"INITIAL DEPOSIT", BigDecimal.valueOf(100), LocalDateTime.now());
+        t = new Transaction(1, a, "INITIAL DEPOSIT", BigDecimal.valueOf(100), LocalDateTime.now());
         transactionDAO.addTransaction(t);
         accountDAO.addAccount(a);
 
         // Account #2 /////////////////////////
-        u=User.parseJson("{\"id\":2,\"userName\":\"Waffle\",\"role\":\"CUSTOMER\",\"accounts\":null}");
+        u = User.parseJson("{\"id\":2,\"userName\":\"Waffle\",\"role\":\"CUSTOMER\",\"accounts\":null}");
         System.out.println("User created from JSON string; id#" + u.getId());
         userDAO.addUser(u);
-        a=Account.parseJson("{\"id\":2}");
+        a = Account.parseJson("{\"id\":2}");
         System.out.println("Account created from JSON string; id#" + a.getId());
         a.setUser(u);
-        t=new Transaction(2,a,"INITIAL DEPOSIT", BigDecimal.valueOf(4), LocalDateTime.now());
+        t = new Transaction(2, a, "INITIAL DEPOSIT", BigDecimal.valueOf(4), LocalDateTime.now());
         transactionDAO.addTransaction(t);
         accountDAO.addAccount(a);
 
         // Account #3 /////////////////////////
-        u=User.parseJson("{\"id\":3,\"userName\":\"Bilbo\",\"role\":\"CUSTOMER\",\"accounts\":null}");
+        u = User.parseJson("{\"id\":3,\"userName\":\"Bilbo\",\"role\":\"CUSTOMER\",\"accounts\":null}");
         System.out.println("User created from JSON string; id#" + u.getId());
         userDAO.addUser(u);
-        a=Account.parseJson("{\"id\":3}");
+        a = Account.parseJson("{\"id\":3}");
         System.out.println("Account created from JSON string; id#" + a.getId());
         a.setUser(u);
-        t=new Transaction(3,a,"INITIAL DEPOSIT", BigDecimal.valueOf(50), LocalDateTime.now());
+        t = new Transaction(3, a, "INITIAL DEPOSIT", BigDecimal.valueOf(50), LocalDateTime.now());
         transactionDAO.addTransaction(t);
         accountDAO.addAccount(a);
 
@@ -92,8 +92,8 @@ public class Main {
         System.out.println("Accounts according to order of priority:");
 
         while (!accounts.isEmpty()) {
-            Account account=accounts.poll();
-            System.out.println("\t"+account.display().replace("\n", "\n\t"));
+            Account account = accounts.poll();
+            System.out.println("\t" + account.display().replace("\n", "\n\t"));
             System.out.println();
             //can also print in JSON
             //System.out.println("JSON:"+account.toJson());
